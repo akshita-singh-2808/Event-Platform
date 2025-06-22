9e69b04cdca87798be83ff8d65350e0ef5e3c09d
Akshita Singh <akshita.singh916@gmail.com>
2025-06-22 19:37:44 +0530

    new file

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -4,26 +4,29 @@ import { WebhookEvent } from '@clerk/nextjs/server'
 import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
 import { verifyWebhook } from '@clerk/nextjs/webhooks';
 import { NextRequest, NextResponse } from 'next/server';
-import { clerkClient } from '@clerk/nextjs/server'
+import clerkClient from '@clerk/clerk-sdk-node';
+
 
 export async function POST(req: NextRequest) {
   try {
     const evt = await verifyWebhook(req);
     // let evt=WebhookEvent
-    const { id } = evt.data;
+    const id = evt.data.id ?? '';
+
     const eventType = evt.type;
 
     if (eventType === 'user.created') {
       const {id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
-    const user = {
-      clerkId: id,
-      email: email_addresses[0].email_address,
-      username: username!,
-      firstName: first_name,
-      lastName: last_name,
-      photo: image_url,
-    }
+          const user = {
+        clerkId: id ?? '',
+        email: email_addresses[0]?.email_address ?? '',
+        username: username ?? '',
+        firstName: first_name ?? '',
+        lastName: last_name ?? '',
+        photo: image_url ?? '',
+      };
+
 
       const newUser = await createUser(user);
 
