36005a90511e27fadd0b737c5492281d5cdbb8ae
Akshita Singh <akshita.singh916@gmail.com>
2025-06-24 23:00:13 +0530

    mongodb connection setup

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher([
-  '/sign-in(.*)',
   '/',
+  '/sign-in(.*)',
   '/events/:id',
-  // '/api/webhook/clerk',
-  // '/api/webhook/stripe',
+  '/api/webhook/clerk',    // ✅ allow webhook to bypass auth
+  '/api/webhook/stripe',   // ✅ optional: allow Stripe webhooks too
   '/api/uploadthing',
   '/assets/videos/video1.mp4',
   '/assets/videos/video2.mp4'
@@ -13,15 +13,13 @@ const isPublicRoute = createRouteMatcher([
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
-    await auth.protect()
+    await auth.protect();
   }
-})
+});
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
-}
+};
